c0693dc1423a3a5e88284b0d992af276d2e90471
Celia <celiamdona@gmail.com>
2022-05-01 12:30:51 -0400

    user routes added

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,4 +1,5 @@
-const User = require('../models');
+const {User} = require('../models');
+
 
 const userController = {
 
@@ -33,9 +34,25 @@ const userController = {
         User.create(body)
         .then(dbUserData => res.json(dbUserData))
         .catch(err => res.json(err));
+    },
+    // update user by id
+    updateUser({params, body}, res){
+        User.findOneAndUpdate({_id: params.id}, body,{new: true})
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id!' });
+                return;
+              }
+              res.json(dbUserData);
+        })
+        .catch(err => res.json(err));
+    },
+    // delete user
+    deleteUser({params}, res){
+        User.findOneAndDelete({_id: params.id})
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.json(err));
     }
-
-
 };
 
 module.exports = userController;
